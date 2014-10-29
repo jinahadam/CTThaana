6d3098ceed430336a6e4c874d04a1800345e3654
Jinah Adam <jinahadam@me.com>
2014-10-29 13:39:57 +0800

    initial commit

--- thaana/ViewController.swift
+++ thaana/ViewController.swift
@@ -7,16 +7,30 @@
 //
 
 import UIKit
+import CoreText
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+       // let font: CTFont = CTFontCreateWithName("AFaseyha" as NSString, 45, nil)
+
+        
+        let font: CTFont = CTFontCreateWithName("MV_Faseyha" as NSString, 45, nil)
+        let text = NSAttributedString(string: "ރާއްޖޭ", attributes: [kCTFontAttributeName: font])
+        label.attributedText = text
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 
